608eabd0b3cebfa36b78a6a3c3c10660eba7e800
Jens Bodal <jensbodal@gmail.com>
2018-01-25 12:31:22 -0800

    add basic setup for dind-docker-compose-aws-terraform image

---
+++ build.sh
@@ -0,0 +1,5 @@
+version="18.01"
+hub_user="jensbodal"
+image="dind-docker-compose-aws-terraform"
+
+docker build "$image" -t "$hub_user/$image" -t "$hub_user/$image:$version" -t "$hub_user/$image:latest"
---
+++ deploy.sh
@@ -0,0 +1,5 @@
+version="18.01"
+hub_user="jensbodal"
+image="dind-docker-compose-aws-terraform"
+
+docker push "$hub_user/$image:$version"
